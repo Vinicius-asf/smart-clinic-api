6ea87064c2dca46567483e9774e7886c9efaf686
brunasqz <bsqueiroz98@gmail.com>
2021-08-03 20:27:09 -0300

    initial routes defined

--- index.js
+++ index.js
@@ -6,20 +6,68 @@ const app = mod_express();
 let port = process.env.PORT ? process.env.PORT : 3001
 console.log(port)
 
-app.get("/", (req, res) => {
-    res.send("Hi there!");
+app.post("/appointment", (req, res) => {
+    res.status(200).send("Successful request");
 });
 
-app.get("/goodbye", (req, res) => {
-    res.send("Goodbye!");
+app.get("/appointment/:id", (req, res) => {
+    res.status(200).send("Successful request");
 });
 
-app.get("/dog", (req, res) => {
-    res.send("Woof!");
+app.patch("/appointment/:id", (req, res) => {
+    res.status(200).send("Successful request");
 });
 
-app.get("*", (req, res) => {
-    res.send("YOU ARE A STAR!");
+app.delete("/appointment/:id", (req, res) => {
+    res.status(200).send("Successful request");
+});
+
+app.get("/appointment/:id/exams", (req, res) => {
+    res.status(200).send("Successful request");
+});
+
+app.post("/appointment/:id/exams", (req, res) => {
+    res.status(200).send("Successful request");
+});
+
+app.get("/clinic/", (req, res) => {
+    res.status(200).send("Successful request");
+});
+
+app.get("/clinic/:id", (req, res) => {
+    res.status(200).send("Successful request");
+});
+
+app.get("/clinic/:id/appointment", (req, res) => {
+    res.status(200).send("Successful request");
+});
+
+app.post("/clinic/:id/patient", (req, res) => {
+    res.status(200).send("Successful request");
+});
+
+app.get("/clinic/:id/patient", (req, res) => {
+    res.status(200).send("Successful request");
+});
+
+app.get("/health/:crm", (req, res) => {
+    res.status(200).send("Successful request");
+});
+
+app.get("/health/", (req, res) => {
+    res.status(200).send("Successful request");
+});
+
+app.get("/patient/:email", (req, res) => {
+    res.status(200).send("Successful request");
+});
+
+app.patch("/patient/:email", (req, res) => {
+    res.status(200).send("Successful request");
+});
+
+app.post("/patient/", (req, res) => {
+    res.status(200).send("Successful request");
 });
 
 app.listen(port, ()=>{
