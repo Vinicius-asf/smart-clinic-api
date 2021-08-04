1a640f727ae2aef8e04d5540cceee9d27f61e800
vinicius-asf <viniciusant3@gmail.com>
2021-08-03 21:38:06 -0300

    patient table created

---
+++ migrations/20210804002919_create_patient_table.js
@@ -0,0 +1,20 @@
+
+exports.up = function (knex) {
+    return knex.schema.createTable('patient', table => {
+        table.string('email').primary();
+        table.string('password').notNull();
+        table.string('name').notNull();
+        table.string('address');
+        table.string('postal_code');
+        table.string('city');
+        table.string('state');
+        table.string('country');
+        table.timestamp('created_at').defaultTo(knex.fn.now());
+        table.timestamp('updated_at');
+        table.timestamp('deleted_at');
+    })
+};
+
+exports.down = function (knex) {
+    return knex.schema.dropTable('patient')
+};
