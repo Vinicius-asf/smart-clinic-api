6e9e87e322f86e10f6a0af59c60428ec9317f797
brunasqz <bsqueiroz98@gmail.com>
2021-08-03 22:25:00 -0300

    adm table created

---
+++ migrations/20210804011446_create_clinic_admnistrative_table.js
@@ -0,0 +1,17 @@
+exports.up = function (knex) {
+    return knex.schema.createTable('admnistrative', table => {
+        table.increments('adm_id');
+        table.integer('clinic_id').notNull();
+        table.foreign('clinic_id').references('clinic.clinic_id').onDelete('RESTRICT');
+        table.string('username').notNull();
+        table.string('password').notNull();
+        table.string('function');
+        table.timestamp('created_at').defaultTo(knex.fn.now());
+        table.timestamp('updated_at');
+        table.timestamp('deleted_at');
+    })
+};
+
+exports.down = function (knex) {
+    return knex.schema.dropTable('administrative')
+};
