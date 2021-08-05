3ac7222a58699f48017f5d6ad7f6549c6e55b119
brunasqz <bsqueiroz98@gmail.com>
2021-08-05 20:30:15 -0300

    healthcare professional table created

---
+++ migrations/20210805231831_create_healthcare_professional_table.js
@@ -0,0 +1,16 @@
+exports.up = function (knex) {
+    return knex.schema.createTable('healthcare_professional', table => {
+        table.string('credential').primary();
+        table.string('name').notNull();
+        table.string('email').notNull().unique();
+        table.string('password').notNull();
+        table.string('profession').notNull();
+        table.timestamp('created_at').defaultTo(knex.fn.now());
+        table.timestamp('updated_at');
+        table.timestamp('deleted_at');
+    })
+};
+
+exports.down = function (knex) {
+    return knex.schema.dropTable('healthcare_professional')
+};
