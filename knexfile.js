034af3a47328d696d310cc4d992636c4f73a76ab
vinicius-asf <viniciusant3@gmail.com>
2021-08-15 13:33:45 -0300

    production knex

--- knexfile.js
+++ knexfile.js
@@ -1,43 +1,43 @@
 //Prod
 
-// module.exports = {
-//   client: 'pg',
-//   development:{
-//     client: 'pg',
-//     seeds:{
-//       directory: './seeds'
-//     }
-//   },
-//   connection: process.env.DATABASE_URL,
-//   pool: {
-//     min: 2,
-//     max: 10
-//   },
-//   migrations: {
-//     tableName: 'knex_migrations'
-//   }
-// };
+module.exports = {
+  client: 'pg',
+  development:{
+    client: 'pg',
+    seeds:{
+      directory: './seeds'
+    }
+  },
+  connection: process.env.DATABASE_URL,
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    tableName: 'knex_migrations'
+  }
+};
 
 //Dev Local
 
-module.exports = {
-    client: 'pg',
-    development:{
-      client: 'pg',
-      seeds:{
-        directory: './seeds'
-      }
-    },
-    connection: {
-      database: 'smart_clinic_db',
-      user: 'postgres',
-      password: '5sWISuDZ2o4LNMSbGtxP'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  };
+// module.exports = {
+//     client: 'pg',
+//     development:{
+//       client: 'pg',
+//       seeds:{
+//         directory: './seeds'
+//       }
+//     },
+//     connection: {
+//       database: 'smart_clinic_db',
+//       user: 'postgres',
+//       password: '5sWISuDZ2o4LNMSbGtxP'
+//     },
+//     pool: {
+//       min: 2,
+//       max: 10
+//     },
+//     migrations: {
+//       tableName: 'knex_migrations'
+//     }
+//   };
