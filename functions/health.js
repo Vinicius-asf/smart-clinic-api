9414b89371cde6e124fe352ccfdb8c5b8e86b78b
brunasqz <bsqueiroz98@gmail.com>
2021-08-17 21:45:44 -0300

    getHealthProfessionalAppointments fix the promises structure

--- functions/health.js
+++ functions/health.js
@@ -39,22 +39,42 @@ module.exports = app => {
     }
 
     // get health worker appointment by credential (crm)
-    const getHealthProfessionalAppointments = (credential) => {
+    const getHealthProfessionalAppointments = async (credential) => {
+        try {
+            const appointments = await app.db('appointment').where({credential});
+            appointments.forEach((appointment, index) => {
+                appointments[index].patient = await getPatientFromAppointment(appointment.patient_email);
+                appointments[index].clinc = await getClinicFromAppointment(appointment.clinic_id);
+            });
+
+            return appointments;
+        }
+        catch(err) {
+            throw Error('error in fetching data\n'+err)
+        }
+    }
+
+    const getPatientFromAppointment = (appointment_patient_email) => {
         return new Promise((resolve, reject) => {
-            app.db('appointment').where({credential})
-            .then(appointment => {
-                app.db('patient').where('email',appointment.patient_email).first()
-                .select('name')
-                .then(patient => {
-                    app.db('clinic').where('clinic_id', appointment.clinic_id).first()
-                    .select('name')
-                    .then(clinic => {
-                        resolve({...appointment, patient:patient, clinic:clinic});
-                    })
-                })
+            app.db('patient').where('email', appointment_patient_email).first()
+            .select('name')
+            .then(queryResult => {
+                resolve(queryResult)
             })
             .catch(err => {
-                reject(Error('error in fetching data\n'+err))
+                reject(Error('error in fetching data \n'+err))
+            });
+        });
+    }
+
+    const getClinicFromAppointment = (appointment_clinic_id) => {
+        return new Promise((resolve, reject) => {
+            app.db('clinic').where('clinic_id', appointment_clinic_id).first()
+            .then(queryResult => {
+                resolve(queryResult)
+            })
+            .catch(err => {
+                reject(Error('error in fetching data \n'+err))
             });
         });
     }
