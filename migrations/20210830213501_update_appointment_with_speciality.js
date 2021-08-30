84b285f9d0127e1c81dc033e7dd218cd55d0d1d3
brunasqz <bsqueiroz98@gmail.com>
2021-08-30 19:17:05 -0300

    fixing migration

--- migrations/20210830213501_update_appointment_with_speciality.js
+++ migrations/20210830213501_update_appointment_with_speciality.js
@@ -2,8 +2,6 @@
 exports.up = function(knex) {
     return knex.schema.table('appointment', table => {
         table.integer('speciality_id');
-        knex('appointment').update({speaciality_id: 1}).where('credential', 12345678);
-        knex('appointment').update({speaciality_id: 6}).where('credential', 33033122);
     });
 };
 
