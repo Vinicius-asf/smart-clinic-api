4fe285659f7cb2a003928658ca49e3c8d4e36c7f
vinicius-asf <viniciusant3@gmail.com>
2021-08-30 21:05:40 -0300

    fix: forgot process.env at service auth keys

--- index.js
+++ index.js
@@ -11,16 +11,16 @@ const { v4: uuidv4 } = require('uuid');
 var admin = require("firebase-admin");
 
 var serviceAccount = process.env.PORT ? {
-    "type": SERVICE_ACCOUNT_TYPE,
-    "project_id": SERVICE_ACCOUNT_PROJECT_ID,
-    "private_key_id": SERVICE_ACCOUNT_PRIVATE_ID,
-    "private_key": SERVICE_ACCOUNT_PRIVATE_KEY,
-    "client_email": SERVICE_ACCOUNT_CLIENT_EMAIL,
-    "client_id": SERVICE_ACCOUNT_CLIENT_ID,
-    "auth_uri": SERVICE_ACCOUNT_AUTH_URI,
-    "token_uri": SERVICE_ACCOUNT_TOKEN_URI,
-    "auth_provider_x509_cert_url": SERVICE_ACCOUNT_CERT_URL,
-    "client_x509_cert_url": SERVICE_ACCOUNT_CLIENT_CERT_URL
+    "type": process.env.SERVICE_ACCOUNT_TYPE,
+    "project_id": process.env.SERVICE_ACCOUNT_PROJECT_ID,
+    "private_key_id": process.env.SERVICE_ACCOUNT_PRIVATE_ID,
+    "private_key": process.env.SERVICE_ACCOUNT_PRIVATE_KEY,
+    "client_email": process.env.SERVICE_ACCOUNT_CLIENT_EMAIL,
+    "client_id": process.env.SERVICE_ACCOUNT_CLIENT_ID,
+    "auth_uri": process.env.SERVICE_ACCOUNT_AUTH_URI,
+    "token_uri": process.env.SERVICE_ACCOUNT_TOKEN_URI,
+    "auth_provider_x509_cert_url": process.env.SERVICE_ACCOUNT_CERT_URL,
+    "client_x509_cert_url": process.env.SERVICE_ACCOUNT_CLIENT_CERT_URL
   } : require("./smart-clinic-pm-firebase-adminsdk.json");
 
 console.log(serviceAccount);
